c0bead4d60c4e7321113a83003df2a522992a7ef
Daniel Javelli <43177050+UNTDaniel2018@users.noreply.github.com>
2021-12-22 12:29:47 -0600

    add problem 1 solution

---
+++ problems/p001.py
@@ -0,0 +1,25 @@
+# Function: Sums multiples of 3 or 5 from 0 to given limit.
+# RETURNS: None
+def calculate_sum(num_limit):
+
+    # set/reset value to 0
+    multiple_sum = 0
+
+    # range starts at 0 goes to num_limit - 1
+    # note 0 gets counted
+    for value in range(num_limit):
+        # check if value is a multple of 3 or 5
+        if (value % 3 == 0 or value % 5 == 0):
+            multiple_sum = multiple_sum + value
+                    
+    # prints results from function
+    print("Sum of all multiples under (%d): %d" % (num_limit, multiple_sum))
+
+
+if __name__ == "__main__":
+    
+    # problem value
+    calculate_sum(1000)
+    
+    # given value, sum = 23
+    calculate_sum(10)
