052b1d03afc7ff6028296296a6d785e9eb7ab1ba
Daniel Javelli <43177050+UNTDaniel2018@users.noreply.github.com>
2021-12-23 15:33:59 -0600

    add solution to probelm 5

--- problems/p005.py
+++ problems/p005.py
@@ -1,10 +1,27 @@
-def smallest_multiple():
+def smallest_multiple(in_num, in_range):
 
     multiples = []
-    
-    return (multiples)
+
+    for index in range(in_range + 1, 1, -1):
+        if (in_num % index == 0):
+            multiples.append(index)
+        else:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
-    smallest_multiple()
+
+    is_done = True
+    in_range = 20
+    in_num = 2520
+
+    while (is_done == True):
+
+        if (smallest_multiple(in_num, in_range) == True):
+            print(in_num)
+            break
+
+        in_num += 2520
 
