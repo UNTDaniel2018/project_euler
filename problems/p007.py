0b4795241b5800630ad28b79e8d907bdcd9d8e90
Daniel Javelli <43177050+UNTDaniel2018@users.noreply.github.com>
2021-12-27 19:48:09 -0600

    add solution to problem 7

---
+++ problems/p007.py
@@ -0,0 +1,27 @@
+def list_primes():
+
+    primes = [2]
+    in_num = 3
+
+    while (len(primes) <= 10001):
+
+        is_prime = True
+
+        for index in range(2, int(in_num/ 2)):
+            if (in_num % index == 0):
+                is_prime = False
+                break
+
+        if(is_prime == True):
+            primes.append(in_num)
+
+        in_num += 2
+
+    return (primes)
+
+
+if __name__ == "__main__":
+
+    prime_num = list_primes()
+
+print(max(prime_num))
